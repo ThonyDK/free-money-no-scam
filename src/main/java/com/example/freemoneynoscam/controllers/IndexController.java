c8b9666588c466ed2625fac37ca85ed9f4f95644
Thony <thon0101@stud.kea.dk>
2021-10-21 10:23:59 +0200

    text added again

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.freemoneynoscam.controllers;
 
+import connector.JDBC;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +12,23 @@ public class IndexController {
     public String index(){
         return "index";
     }
-
+    //postmapping er hvis vi skal have data som personlig oplysninger submittet fra hjemmeside via springboot og
+    //videre til database.
     @PostMapping("/test")
     public String userEmail(WebRequest wr){
         String parameter = wr.getParameter("test-input");
         System.out.println(parameter);
+        JDBC connection = new JDBC();
+        connection.connectionTest(parameter);
         return "index";
     }
-    //Hello
-
+    //Getmapping er hvis vi skal hente data fra clienten/Browser til en database.
+    //altså hvor det fx ikke er personlige oplysninger.
+    @GetMapping("/test")
+    public String test(){
+        JDBC connection = new JDBC();
+        //connection.connectionTest();
+        return "index";
+    }
+    //Helloworld
 }
---
+++ src/main/java/connector/JDBC.java
@@ -0,0 +1,23 @@
+package connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+//Connector laver forbindelse til database så alt data der er skrevet ind i indskrivningsfeltet bliver sendt videre
+//til databasen.
+public class JDBC {
+    public void connectionTest(String emailFromForm) {
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/emaildatabase","Kurt","x");
+            PreparedStatement psts = conn.prepareStatement("insert into customer(email) values(?)");
+            psts.setString(1,emailFromForm);
+            psts.execute();
+
+        }catch(SQLException e){
+            System.out.println("Cannot connect to database");
+            e.printStackTrace();
+        }
+    }
+}
